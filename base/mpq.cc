c3679bd904e1513a72ad8000167121ed4758899d
lenxyang <lenxyang@gmail.com>
2014-08-11 17:53:47 +0800

    .

--- base/mpq.cc
+++ base/mpq.cc
@@ -1,1 +1,22 @@
 #include "wow/base/mpq.h"
+#include "Stormlib.h"
+#include "base/strings/utf_string_conversions.h"
+
+bool MPQFile::open() {
+  return true;
+}
+
+
+MPQArchive::MPQArchive(const ::base::FilePath& filepath)
+    : filepath_(filepath)
+    , handle_(-1) {
+}
+
+bool MPQArchive::open() {
+  std::string filepath = ::base::WideToUTF8(filepath_);
+  if (!SFileOpenArchive(filepath, 0,
+                        MPQ_OPEN_FORCE_MPQ_V1 | MPQ_OPEN_READ_ONLY,
+                        &handle_)) {
+    return false;
+  }
+}
--- base/mpq.h
+++ base/mpq.h
@@ -1,3 +1,36 @@
 #pragma once
 
+#include <string>
+#include <memory>
 #include "base/basictypes.h"
+#include "base/files/file_path.h"
+
+class MPQFile {
+ public:
+  MPQFile(const std::string& filepath)
+      : path_(filepath) {
+  }
+
+  bool open();
+ private:
+  std::string path_;
+  std::unique_ptr<char[]> buffer_;
+  int64 size_;
+  DISALLOW_COPY_AND_ASSIGN(MPQFile);
+};
+
+class MPQArchive {
+ public:
+  MPQArchive(const base::FilePath& filepath);
+  bool open();
+  bool IsPartical();
+
+  bool ExistsFile(const std::string& filepath);
+  bool ReadFile(const std::string& filepath, char* buffer, int size);
+  int64 GetFileSize(const std::string& filepath);
+ private:
+  typedef int HANDLE;
+  HANDLE handle_;
+  ::base::FilePath filepath_;
+  DISALLOW_COPY_AND_ASSIGN(MPQArchive);
+};
